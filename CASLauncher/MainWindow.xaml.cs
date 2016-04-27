b16821157d3394b8a9fc68a427a6ac0eb3e8a93d
Дмитрий Строкин <nekromant8818@mail.ru>
2016-04-27 16:49:26 +0400

    Добавлены проекты лаунчера системы и библиотеки классов

---
+++ CASLauncher/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: paladin
+ * Дата: 27.04.2016
+ * Время: 16:45
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CASLauncher
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
